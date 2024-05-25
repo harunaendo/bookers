883ca6f8f021c75c4866aa89c909a769565a79f9
haruna endo <163505612+harunaendo@users.noreply.github.com>
2024-05-25 11:04:54 +0000

    start

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   get 'books/new'
   post 'books' => 'books#create'
   #showアクション削除
-  get 'books/:id' => 'books#show'
-  get 'books/edit'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get 'homes/top'
   get 'books' => 'books#index'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
   root :to => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
