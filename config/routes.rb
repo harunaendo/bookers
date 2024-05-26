0302ab9c27b31576c7e359d7a2d898bc40e59bc5
haruna endo <163505612+harunaendo@users.noreply.github.com>
2024-05-26 05:33:50 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   post 'books' => 'books#create'
   #showアクション削除
+  get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get 'homes/top'
-  get 'books' => 'books#index'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
